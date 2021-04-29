66cb82bd155080b511d3b6877b94cf7bb907dd49
Bruna Torres <53224985+bruninhaltorres@users.noreply.github.com>
2021-04-29 20:24:18 -0300

    Venda e Cartão de Ponto adicionado

--- Main.java
+++ Main.java
@@ -3,14 +3,11 @@ import src.employees.Employees;
 import src.employees.Syndicate;
 import src.employees.Hourly;
 import src.employees.Syndicate;
-import src.TimeCard;
 
 import java.util.Scanner;
 import java.lang.String;
 import java.util.Random;
 import java.util.ArrayList;
-
-
 
 public class Main {
 
@@ -38,10 +35,7 @@ public class Main {
         }
         return id;
     }
-    public static void main(String[] args) {
-        System.out.println("Esse é o sistema para folha de pagamento.");    
-        Scanner input = new Scanner(System.in);
-
+    public static void actions(){
         System.out.println("Ações:"); // printar aos poucos?
         System.out.println("1 - Adicionar empregado"); 
         System.out.println("2 - Remoção empregado"); 
@@ -54,12 +48,14 @@ public class Main {
         System.out.println("9 - Agendar pagamento");
         System.out.println("10 - Criar novas agendas");
         System.out.println("0 - Encerrar");
-
+    }
+    public static void main(String[] args) {
+        System.out.println("Esse é o sistema para folha de pagamento.");    
+        Scanner input = new Scanner(System.in);
+        actions();
         int comando = input.nextInt();
-
         ArrayList<Employees> listEmployees = new ArrayList<Employees>(); 
         ArrayList<Syndicate> listSyndicate = new ArrayList<Syndicate>(); 
-
 
         while (comando != 0){
             if (comando == 1) {
@@ -69,19 +65,21 @@ public class Main {
 
                 Employees employees = new Employees(idEmployee);
                 employees.add();
-                listEmployees.add(employees);
 
                 System.out.println("O empregado é...\n1 - Horista\n2 - Comissionado");
                 int type = input.nextInt();
                 
                 if (type == 1) { // horista
-                    System.out.println("Então preciso de mais algumas informações");
-                    Hourly hourly = new Hourly(employees.getName(), employees.getAdress(), employees.getMethodPayment(), employees.getId());                    hourly.add();
+                    //System.out.println("Então preciso de mais algumas informações");
+                    Hourly hourly = new Hourly(employees.getName(), employees.getAdress(), employees.getMethodPayment(), employees.getId());                    
+                    listEmployees.add(hourly);
+                    // hourly.add();
                 } if (type == 2) {
-                    int comissioned;
+                    double valueComissioned;
                     System.out.println("Valor da comissão:");
-                    comissioned = input.nextInt();
-                    Commissioned commissioned = new Commissioned(employees.getName(), employees.getAdress(), employees.getMethodPayment(), employees.getId(), committee)
+                    valueComissioned = input.nextDouble();
+                    Commissioned commissioned = new Commissioned(employees.getName(), employees.getAdress(), employees.getMethodPayment(), employees.getId(), valueComissioned);
+                    listEmployees.add(commissioned);
                 }
 
                 System.out.println("Ele irá fazer parte do Sindicato?\n1 - Sim\n2 - Não");
@@ -91,30 +89,91 @@ public class Main {
                     Syndicate syndicate = new Syndicate(idSyndicate);
                     syndicate.add(idEmployee);
                     listSyndicate.add(syndicate);
-                    // Syndicate add_syndicate = new Syndicate(employees.getName(), employees.getAdress(), employees.getMethodPayment(), employees.getId()); // como usar o array de empregados aqui?
-                } 
-                System.out.println("Empregado adicionado com sucesso!");
+                }
+                System.out.println("Id adicionado: " + idEmployee);
+                for (Employees employeesOk : listEmployees) {
+                    System.out.println("Empregado " + employeesOk.getName() + " adicionado com sucesso!");
+                    /*if(employeesOk.getId() == idEmployee) {
+                        System.out.println("Empregado " + employeesOk.getName() + " adicionado com sucesso!");
+                    }*/
+                }
             } else if (comando == 2) {
                 System.out.println("Qual o identificador do empregado que você deseja remover?");
                 int idRemove = input.nextInt();
-                
-                for (Employees employees : listEmployees) {
-                    if(employees.getId() == idRemove) {
-                        listEmployees.remove(employees);
+                Employees employeeRemove = null;
+                for(Employees employees : listEmployees){
+                    if(employees.getId() == idRemove){
+                        employeeRemove = employees;
                     }
                 }
-                for (Syndicate syndicate : listSyndicate) { 
-                    if(syndicate.getIdSyndicate() == idRemove) {
-                        listSyndicate.remove(syndicate);
+                listEmployees.remove(employeeRemove);
+
+                Syndicate syndicateRemove = null;
+                for(Syndicate syndicate : listSyndicate){
+                    if(syndicate.getIdSyndicate() == idRemove){
+                        syndicateRemove = syndicate;
                     }
                 }
+                listSyndicate.remove(syndicateRemove);
+
+                /*
+                List<Employees> employeeRemove = new ArrayList<Employees>();
+                for(Employees employees : listEmployees){
+                    if(employees.getId() == idRemove){
+                        employeeRemove.add(employees);
+                    }
+                }
+                listEmployees.removeAll(employeeRemove);
+                */
                 System.out.println("Removendo empregado..."); 
+
+                System.out.println("Todos os empregados: ");
+                for (Employees employeesOk : listEmployees) {
+                    System.out.println(employeesOk.getName());
+                }
+
                 System.out.println("Empregado removido!"); 
             } else if (comando == 3) {
-                //
+                System.out.println("Qual seu numero de identificação?");
+                int idTimeCard = input.nextInt();
+                /*
+                int size = listEmployees.size();
+                for(int i = 0; i < size; i++) {
+                    if(listEmployees.get(i).getId() == idTimeCard) {
+                        if(listEmployees.get(i).getClass() == Hourly.class) {
+                            ((Hourly)listEmployees.get(i)).addTimeCard(); // A posição i da lista é convertida para Houyrly nesse momento. Depois volta a ser Employee. 
+                        }
+                    }
+                }
+                */
+                for(Employees employees : listEmployees){
+                    if(employees.getId() == idTimeCard){
+                        if(employees.getClass() == Hourly.class){
+                            ((Hourly)employees).addTimeCard();
+                        }
+                    }
+                }
                 System.out.println("Cartão de ponto lançado!");
             } else if (comando == 4) {
-                //
+                System.out.println("Qual seu numero de identificação?");
+                int idVendedor = input.nextInt();
+                /*
+                int size = listEmployees.size();
+                for(int i = 0; i < size; i++) {
+                    if(listEmployees.get(i).getId() == idTimeCard) {
+                        if(listEmployees.get(i).getClass() == Hourly.class) {
+                            ((Hourly)listEmployees.get(i)).addTimeCard(); // A posição i da lista é convertida para Houyrly nesse momento. Depois volta a ser Employee. 
+                        }
+                    }
+                }
+                */
+                for(Employees employees : listEmployees){
+                    if(employees.getId() == idVendedor){
+                        if(employees.getClass() == Commissioned.class){
+                            ((Commissioned)employees).addSale();
+                        }
+                    }
+                }
                 System.out.println("Venda cadastrada!");
             } else if (comando == 5) {
                 //
@@ -137,17 +196,7 @@ public class Main {
             System.out.println("11 - Ver as opções novamente\n0 - Encerrar");
             comando = input.nextInt();
             if (comando == 11) {
-                System.out.println("1 - Adicionar empregado"); 
-                System.out.println("2 - Remoção empregado"); 
-                System.out.println("3 - Lançar um cartão"); 
-                System.out.println("4 - Lançar resultado venda");
-                System.out.println("5 - Lançar taxa serviço"); 
-                System.out.println("6 - Alterar detalhes"); 
-                System.out.println("7 - Folha de pagamento para hoje");
-                System.out.println("8 - Undo/redo");
-                System.out.println("9 - Agendar pagamento");
-                System.out.println("10 - Criar novas agendas");
-                System.out.println("0 - Encerrar");
+                actions();
             }
             input.nextLine();
         }
