ac5bc900cc807b75e8d14f9cf1bff686634451a1
Bruna Torres <53224985+bruninhaltorres@users.noreply.github.com>
2021-04-29 20:26:06 -0300

    Venda e cartão de ponto adicionado

--- src/employees/Hourly.java
+++ src/employees/Hourly.java
@@ -2,9 +2,7 @@ package src.employees;
 
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.sql.Time;
 import src.TimeCard;
-
 
 public class Hourly extends Employees {
     private ArrayList<TimeCard> listTimeCards = new ArrayList<TimeCard>(); // um horista tem varios cartões de ponto
@@ -13,23 +11,25 @@ public class Hourly extends Employees {
         super(name, adress, method, id);// chama o construtor da classe pai.
     }
 
-    public void addTimeCard(TimeCard timeCard){
-        listTimeCards.add(timeCard);
-    }
-
     public void getHoursWeekly(TimeCard timeCard){
         timeCard.setHoursWeekly(listTimeCards);
     }
 
-    public void add(){
+    public void addTimeCard(){
         Scanner input = new Scanner(System.in);
         System.out.println("Data: (DD/MM/AA)");
         String date = input.nextLine();
+
         System.out.println("Hora de entrada: (24h)");
         int in = input.nextInt();
+
         System.out.println("Hora de saida: (24h)");
         int out = input.nextInt();
+        
         TimeCard timeCard = new TimeCard(in, out, date);
-        addTimeCard(timeCard);
+        listTimeCards.add(timeCard);
+        for(TimeCard timecard : listTimeCards){
+            System.out.println("Data adicionada: " + timecard.getDate());
+        }
     }
 }
